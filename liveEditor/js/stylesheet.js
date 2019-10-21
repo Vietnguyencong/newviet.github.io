285963abd7378fbad5908ee512074e176d6ea019
Vietnguyencong <vietnguyencong99@gmail.com>
2019-10-21 15:12:27 -0500

    eliminate "/"

--- liveEditor/js/stylesheet.js
+++ liveEditor/js/stylesheet.js
@@ -3,21 +3,21 @@
 
 
 function oringinalView(){
-    document.getElementById('view').setAttribute('href','/liveEditor/view/originalview.css')
+    document.getElementById('view').setAttribute('href','liveEditor/view/originalview.css')
 }
 function squareView(){
-    document.getElementById('view').setAttribute('href','/liveEditor/view/squareview.css')
+    document.getElementById('view').setAttribute('href','liveEditor/view/squareview.css')
 }
 function verticalViewHTML(){
-    document.getElementById('view').setAttribute('href','/liveEditor/view/verticalViewHTML.css')
+    document.getElementById('view').setAttribute('href','liveEditor/view/verticalViewHTML.css')
 
 }
 function verticalViewCSS(){
-    document.getElementById('view').setAttribute('href','/liveEditor/view/verticalViewCSS.css')
+    document.getElementById('view').setAttribute('href','liveEditor/view/verticalViewCSS.css')
 
 }
 function verticalViewJS(){  
-    document.getElementById('view').setAttribute('href','/liveEditor/view/verticalViewJS.css')
+    document.getElementById('view').setAttribute('href','liveEditor/view/verticalViewJS.css')
 
 }
 function showDropDown(){  
